a0ea6bd5449f893ab5a980d2ac4863433ee6c5c6
Sangeetha Rajeev <sang.rajeev@gmail.com>
2018-04-11 04:38:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   array = sentence.split(" ")
   
   reverse_array = []
-  array.each do |word|
+  array.collect do |word|
     reverse_array.push(word.reverse)
   end
   
